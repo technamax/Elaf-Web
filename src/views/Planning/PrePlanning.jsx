7d3e080b6d3bdf9465a7761d3b4d412d18e2b747
Junaid Ashraf <junaid.ashraf@technamax.com>
2024-05-23 17:34:13 +0500

    preplaaning save data

--- src/views/Planning/PrePlanning.jsx
+++ src/views/Planning/PrePlanning.jsx
@@ -27,18 +27,21 @@ const PrePlanning = () => {
   const [colors, setColors] = useState([]);
   const [uoms, setUoms] = useState([]);
   const [heads, setHeads] = useState([]);
-  console.log('components:', components);
-  console.log('Fabrications:', Fabrications);
-  console.log('colors:', colors);
-  console.log('uoms:', uoms);
-  console.log('heads:', heads);
+  const [batchList, setBatchList] = useState([]);
+  // console.log('components:', components);
+  // console.log('Fabrications:', Fabrications);
+  // console.log('colors:', colors);
+  // console.log('uoms:', uoms);
+  // console.log('heads:', heads);
 
   const [formData, setFormData] = useState({
     collectionId: '',
     baseColorId: '', // not in api
+    baseColorName: '', // not in api
     noOfDesigns: '', // not in apis
     noOfColors: '', // not in api
     designId: '',
+    batchNo: '',
     componentId: '',
     cuttingSize: '', // not in api
     colorId: '',
@@ -78,10 +81,80 @@ const PrePlanning = () => {
     fetchPrePlanningLookUp();
   }, []);
 
+  useEffect(() => {
+    const GetPrePlanningHeaderByDesignId = async (id) => {
+      try {
+        const response = await axios.get(
+          `https://gecxc.com:4041/api/PrePlanning/GetPrePlanningHeaderByDesignId?designId=${id}`
+        );
+        console.log(response.data);
+        setBatchList(response.data);
+      } catch (error) {
+        console.error('Error fetching pre-planning lookup data:', error);
+      }
+    };
+
+    GetPrePlanningHeaderByDesignId(formData.designId);
+  }, [formData.designId]);
+  console.log('batchList', batchList);
+
+  useEffect(() => {
+    const calculateTotalFabric = () => {
+      const repeats = parseFloat(formData.repeats) || 0;
+      const repeatSize = parseFloat(formData.repeatSize) || 0;
+      return repeats * repeatSize;
+    };
+
+    setFormData((prevData) => ({
+      ...prevData,
+      totalFabric: calculateTotalFabric()
+    }));
+    const calculateTotal = () => {
+      const totalFabric = parseFloat(formData.totalFabric) || 0;
+      const shrinkage = parseFloat(formData.shrinkage) || 0;
+      const wastage = parseFloat(formData.wastage) || 0;
+      return (totalFabric * (100 - (shrinkage + wastage))) / 100;
+    };
+
+    setFormData((prevData) => ({
+      ...prevData,
+      total: calculateTotal()
+    }));
+  }, [
+    formData.repeats,
+    formData.repeatSize,
+    formData.totalFabric,
+    formData.shrinkage,
+    formData.wastage
+  ]);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+    if (name === 'collectionId') {
+      const selectedCollection = collectionList.find(
+        (collection) => collection.collectionId === parseInt(value)
+      );
+      setFormData({
+        ...formData,
+        collectionId: value,
+        noOfDesigns: selectedCollection ? selectedCollection.noOfDesigns : '',
+        noOfColors: selectedCollection ? selectedCollection.noOfColors : ''
+      });
+    } else if (name === 'designId') {
+      const selectedDesign = designList.find(
+        (design) => design.designId === parseInt(value)
+      );
+      setFormData({
+        ...formData,
+        designId: value,
+        baseColorId: selectedDesign ? selectedDesign.colorId : '',
+        baseColorName: selectedDesign ? selectedDesign.colorName : ''
+      });
+    } else {
+      setFormData({ ...formData, [name]: value });
+    }
   };
+  // console.log('noOfDesigns', formData.noOfDesigns); colorId
 
   const initialRows = [];
 
@@ -135,6 +208,10 @@ const PrePlanning = () => {
       // }))
     }
   ];
+  console.log('designList:', designList);
+  const handleSave = () => {
+    console.log(formData);
+  };
 
   return (
     <MainCard
@@ -152,7 +229,7 @@ const PrePlanning = () => {
             </Typography>
           </Grid>
           <Grid item sm={3} textAlign="right">
-            <Button variant="contained" size="small">
+            <Button variant="contained" size="small" onClick={handleSave}>
               Save
             </Button>
           </Grid>
@@ -160,19 +237,20 @@ const PrePlanning = () => {
             <TextField
               fullWidth
               select
-              label="Select Design"
-              name="designId"
-              value={formData.designId}
+              label="Select Collection"
+              name="collectionId"
+              value={formData.collectionId}
               onChange={handleChange}
               size="small"
             >
-              {designList.map((option) => (
-                <MenuItem key={option.designId} value={option.designId}>
-                  {option.designerName}
+              {collectionList.map((option) => (
+                <MenuItem key={option.collectionId} value={option.collectionId}>
+                  {option.collectionName}
                 </MenuItem>
               ))}
-            </TextField>
+            </TextField>{' '}
           </Grid>
+
           <Grid item sm={4}>
             <TextField
               label="No of Design"
@@ -193,6 +271,54 @@ const PrePlanning = () => {
               onChange={handleChange}
             />
           </Grid>
+          <Grid item sm={4}>
+            <TextField
+              fullWidth
+              select
+              label="Select Design"
+              name="designId"
+              value={formData.designId}
+              onChange={handleChange}
+              size="small"
+            >
+              {designList.map((option) => (
+                <MenuItem key={option.designId} value={option.designId}>
+                  {option.designerName}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
+          <Grid item sm={4}>
+            <TextField
+              label="Base Color"
+              fullWidth
+              size="small"
+              name="baseColorName"
+              value={formData.baseColorName}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item sm={4}>
+            <TextField
+              fullWidth
+              select
+              label="Batch No."
+              name="batchNo"
+              value={formData.batchNo}
+              onChange={handleChange}
+              size="small"
+            >
+              {batchList.map((option) => (
+                <MenuItem
+                  key={option.planningHeaderId}
+                  value={option.planningHeaderId}
+                >
+                  {option.batchNo}
+                </MenuItem>
+              ))}
+            </TextField>{' '}
+          </Grid>
+
           <Grid item sm={12}>
             <Divider></Divider>
           </Grid>
@@ -280,7 +406,7 @@ const PrePlanning = () => {
           </Grid>
           <Grid item sm={3}>
             <TextField
-              label="Repeats "
+              label="Repeats"
               fullWidth
               size="small"
               name="repeats"
@@ -300,9 +426,20 @@ const PrePlanning = () => {
           </Grid>
           <Grid item sm={3}>
             <TextField
-              label="UOM "
+              label="Total Fabric"
+              fullWidth
+              size="small"
+              name="totalFabric"
+              value={formData.totalFabric}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item sm={3}>
+            <TextField
               fullWidth
               select
+              label="UOM"
+              defaultValue=""
               size="small"
               name="uomId"
               value={formData.uomId}
@@ -317,17 +454,7 @@ const PrePlanning = () => {
           </Grid>
           <Grid item sm={3}>
             <TextField
-              label="Total Fabric"
-              fullWidth
-              size="small"
-              name="totalFabric"
-              value={formData.totalFabric}
-              onChange={handleChange}
-            />
-          </Grid>
-          <Grid item sm={3}>
-            <TextField
-              label="Shrinkage"
+              label="Shrinkage %"
               fullWidth
               size="small"
               name="shrinkage"
@@ -337,7 +464,7 @@ const PrePlanning = () => {
           </Grid>
           <Grid item sm={3}>
             <TextField
-              label="Wastage"
+              label="Wastage %"
               fullWidth
               size="small"
               name="wastage"
@@ -357,9 +484,7 @@ const PrePlanning = () => {
           </Grid>
         </Grid>
       </FormControl>
-      <Grid item sm={12} mt={3}>
-        <EditAbleDataGrid initialRows={initialRows} ncolumns={columns} />
-      </Grid>
+      <EditAbleDataGrid ncolumns={columns} initialRows={initialRows} />
     </MainCard>
   );
 };
