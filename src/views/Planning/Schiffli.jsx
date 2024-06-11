29c80ae429ac1934506602b177841120a7f589ef
Mohsin-Shafique <164845524+Mohsin-Shafique@users.noreply.github.com>
2024-06-11 17:38:53 +0500

    merge fixes

--- src/views/Planning/Schiffli.jsx
+++ src/views/Planning/Schiffli.jsx
@@ -38,7 +38,12 @@ import { useGetPrePlanningHeaderByDesignIdQuery } from 'api/store/Apis/prePlanni
 import { useGetLookUpListQuery } from 'api/store/Apis/lookupApi';
 import { useGetComponentsByBatchNoQuery } from 'api/store/Apis/prePlanningHeaderApi';
 import { useGetFabricByComponentsAndBatchNoQuery } from 'api/store/Apis/prePlanningHeaderApi';
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
+import SendAndArchiveIcon from '@mui/icons-material/SendAndArchive';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+import schiffli from '../../assets/images/planningicons/schiffli.png';
 import EditAbleDataGrid from 'components/EditAbleDataGrid';
 import MainCard from 'ui-component/cards/MainCard';
 import { AlignHorizontalCenter } from '@mui/icons-material';
@@ -116,7 +121,7 @@ const Schiffli = () => {
   }, [initialData]);
   const [accordionExpanded, setAccordionExpanded] = useState(false); // Add state variable for accordion
   const handleAccordionToggle = (event, isExpanded) => {
-    setAccordionExpanded(isExpanded);
+    setAccordionExpanded(!accordionExpanded);
   };
 
   const { data: collectionData } = useGetCollectionFromPlanningHeaderQuery();
@@ -627,7 +632,7 @@ const Schiffli = () => {
       <Card variant="outlined">
         <CardHeader
           className="css-4rfrnx-MuiCardHeader-root"
-          // avatar={<Avatar src={dyeing} sx={{ background: 'transparent' }} />}
+          avatar={<Avatar src={schiffli} sx={{ background: 'transparent' }} />}
           title="Schiffli "
           titleTypographyProps={{ style: { color: 'white' } }}
         ></CardHeader>
@@ -719,11 +724,11 @@ const Schiffli = () => {
               disabled
             />
           </Grid>
-          <Grid item xs={12} textAlign="right">
+          {/* <Grid item xs={12} textAlign="right">
             <Button variant="contained" size="small" onClick={handleSave}>
               Save
             </Button>
-          </Grid>
+          </Grid> */}
         </Grid>
       </Card>
       <Divider color="#cc8587" sx={{ height: 1, width: '100%', mt: 2 }} />
@@ -731,10 +736,20 @@ const Schiffli = () => {
       <Card variant="outlined">
         <CardHeader
           className="css-4rfrnx-MuiCardHeader-root"
-          // avatar={<Avatar src={dyeing} sx={{ background: 'transparent' }} />}
+          avatar={<AddOutlinedIcon />}
           title="Add Schiffli "
           titleTypographyProps={{ style: { color: 'white' } }}
-        ></CardHeader>
+          action={
+            <IconButton onClick={handleAccordionToggle}>
+              <ExpandMoreIcon style={{ color: 'white' }} />
+            </IconButton>
+          }
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between'
+          }}
+        ></CardHeader>{' '}
         <Accordion
           expanded={accordionExpanded}
           onChange={handleAccordionToggle}
@@ -744,6 +759,7 @@ const Schiffli = () => {
             expandIcon={<ExpandMoreIcon />}
             aria-controls="panel1-content"
             id="panel1-header"
+            sx={{ display: 'none' }} // Hide this since we're using the icon in the CardHeader
           ></AccordionSummary>
           <AccordionDetails>
             <Grid
@@ -1015,6 +1031,11 @@ const Schiffli = () => {
                   </Grid>
                 </Grid>
               ) : null}
+              <Grid item xs={12} textAlign="right" sx={{ mt: 2 }}>
+                <Button variant="contained" size="small" onClick={handleSave}>
+                  Save
+                </Button>
+              </Grid>
             </Grid>
           </AccordionDetails>
         </Accordion>
@@ -1024,7 +1045,7 @@ const Schiffli = () => {
       <Card variant="outlined">
         <CardHeader
           className="css-4rfrnx-MuiCardHeader-root"
-          // avatar={<Avatar src={dyeing} sx={{ background: 'transparent' }} />}
+          avatar={<VisibilityOutlinedIcon />}
           title="View Schiffli "
           titleTypographyProps={{ style: { color: 'white' } }}
         ></CardHeader>
