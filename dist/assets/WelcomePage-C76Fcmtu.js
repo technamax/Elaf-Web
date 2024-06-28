28504f86cabbc3c0f42444e3d1e94487c69a1b89
Mohsin-Shafique <164845524+Mohsin-Shafique@users.noreply.github.com>
2024-06-28 09:39:51 +0500

    build

---
+++ dist/assets/WelcomePage-C76Fcmtu.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-C1TTrB0-.js";import"./vendor-BXCbSo2_.js";function t(){return e.jsx("div",{children:"WelcomePage"})}export{t as default};
--- dist/assets/WelcomePage-DzhcvIDa.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-r6CzGbM1.js";import"./vendor-BXCbSo2_.js";function t(){return e.jsx("div",{children:"WelcomePage"})}export{t as default};
