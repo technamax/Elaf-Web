a29796a24db9aadc6c40e7de5d17904e0c7fe3a6
Mohsin-Shafique <164845524+Mohsin-Shafique@users.noreply.github.com>
2024-07-15 14:56:52 +0500

    Added Production Menu

--- src/menu-items/index.js
+++ src/menu-items/index.js
@@ -1,17 +1,14 @@
-
-
-
-
 import dashboard from './dashboard';
 import pages from './pages';
 import utilities from './utilities';
 import other from './other';
 import Planning from './Planning';
+import Production from './Production';
 
 // ==============================|| MENU ITEMS ||============================== //
 
 const menuItems = {
-  items: [dashboard, Planning, other, pages]
+  items: [dashboard, Planning, Production, other, pages]
 };
 
 export default menuItems;
