b19610ce1fdfeb54f9f7e7f04b399fc333ffcf2a
Mohsin-Shafique <164845524+Mohsin-Shafique@users.noreply.github.com>
2024-07-19 12:00:57 +0500

    no message

--- src/views/Planning/Summary.jsx
+++ src/views/Planning/Summary.jsx
@@ -1,67 +1,223 @@
-import { useState, useEffect, useCallback } from 'react';
-
-import axios from 'axios';
+import { useState, useEffect } from 'react';
 import {
-  Button,
   MenuItem,
-  FormControl,
-  Typography,
-  Divider,
-  Grid,
   TextField,
-  Box,
-  Tab,
+  Grid,
   Card,
   CardHeader,
-  IconButton
+  Divider
 } from '@mui/material';
-import MainCard from 'ui-component/cards/MainCard';
+import {
+  useGetCollectionFromPlanningHeaderQuery,
+  useGetPrePlanningHeaderByDesignIdQuery,
+  useGetFabricFromPrePlanningByBatchNoQuery,
+  useGetFabricRequisitionListByBatchNoQuery,
+  useGetAdditionalProcessListByBatchNoQuery,
+  useGetFabricColorByComponentsBatchNoAndFabricIdQuery
+} from 'api/store/Apis/prePlanningHeaderApi';
+import { useGetDesignFromPlanningHeaderByCollectionIdQuery } from 'api/store/Apis/prePlanningHeaderApi';
+import { useSnackbar } from 'notistack';
+import { useUser } from 'context/User';
 
-const Summary = () => {
+const Summary = (initialValues) => {
+  const { user } = useUser();
+  const { enqueueSnackbar } = useSnackbar();
+  const [initialData, setInitialData] = useState([]);
+  const { data: collectionData } = useGetCollectionFromPlanningHeaderQuery();
+  const [selectedCollectionId, setSelectedCollectionId] = useState('');
+  const [formData, setFormData] = useState({
+    designId: '',
+    planningHeaderId: 0,
+    batchNo: ''
+  });
+
+  useEffect(() => {
+    setSelectedCollectionId(initialValues.collectionId);
+  }, [initialValues]);
+
+  const { data: designData, refetch } =
+    useGetDesignFromPlanningHeaderByCollectionIdQuery(selectedCollectionId, {
+      skip: !selectedCollectionId // Skip the query if no collection is selected
+    });
+
+  const { data: batchData, refetch: refetchBatches } =
+    useGetPrePlanningHeaderByDesignIdQuery(formData.designId, {
+      skip: !formData.designId // Skip the query if no design is selected
+    });
+
+  const [designList, setDesignList] = useState([]);
+  const [batchList, setBatchList] = useState([]);
+  const [collectionList, setCollectionList] = useState([]);
+
+  useEffect(() => {
+    if (collectionData) {
+      setCollectionList(collectionData.result);
+    }
+  }, [collectionData]);
+
+  useEffect(() => {
+    if (designData) {
+      setDesignList(designData.result);
+    }
+  }, [designData]);
+
+  useEffect(() => {
+    if (batchData) {
+      setBatchList(batchData.result);
+    }
+  }, [batchData]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    console.log(`handleChange called with name: ${name}, value: ${value}`);
+
+    if (name === 'collectionId') {
+      setSelectedCollectionId(value);
+      setFormData({
+        ...formData,
+        collectionId: value,
+        designId: '',
+        planningHeaderId: 0,
+        batchNo: ''
+      });
+    } else if (name === 'designId') {
+      setFormData({
+        ...formData,
+        designId: value,
+        planningHeaderId: 0
+      });
+    } else if (name === 'batchNo') {
+      const selectedBatch = batchList.find((batch) => batch.batchNo === value);
+      setFormData({
+        ...formData,
+        batchNo: value,
+        planningHeaderId: selectedBatch ? selectedBatch.planningHeaderId : ''
+      });
+    } else {
+      setFormData({ ...formData, [name]: value });
+    }
+  };
+
+  console.log('Selected collection ID:', selectedCollectionId);
+  console.log('Form data:', formData);
+  console.log('Design list:', designList);
+  console.log('Batch list:', batchList);
+
   return (
     <>
       <Card variant="outlined">
         <CardHeader
           className="css-4rfrnx-MuiCardHeader-root"
-          //   avatar={<Avatar src={roller} sx={{ background: 'transparent' }} />}
-          title="View Summary "
+          title="View Summary"
           titleTypographyProps={{ style: { color: 'white' } }}
-        ></CardHeader>
+        />
+        <Grid
+          container
+          spacing={1}
+          width="inherit"
+          sx={{ paddingY: 2, paddingX: 2 }}
+        >
+          <Grid item xs={12} md={3}>
+            <TextField
+              fullWidth
+              select
+              label="Select Collection"
+              name="collectionId"
+              value={selectedCollectionId}
+              onChange={handleChange}
+              size="small"
+              InputLabelProps={{
+                sx: {
+                  color: 'black'
+                }
+              }}
+            >
+              {collectionList.map((option) => (
+                <MenuItem key={option.collectionId} value={option.collectionId}>
+                  {option.collectionName}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <TextField
+              fullWidth
+              select
+              label="Select Design"
+              name="designId"
+              value={formData.designId}
+              onChange={handleChange}
+              size="small"
+              InputLabelProps={{
+                sx: {
+                  color: 'black'
+                }
+              }}
+            >
+              {designList.map((option) => (
+                <MenuItem key={option.designId} value={option.designId}>
+                  {option.designNo}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <TextField
+              fullWidth
+              select
+              label="Batch No."
+              name="batchNo"
+              value={formData.batchNo}
+              onChange={handleChange}
+              size="small"
+              InputLabelProps={{
+                sx: {
+                  color: 'black'
+                }
+              }}
+            >
+              {batchList.map((option) => (
+                <MenuItem key={option.batchNo} value={option.batchNo}>
+                  {option.batchNo}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
+        </Grid>
       </Card>
 
       <Grid item xs={12} md={12}>
         <Divider
           color="#921e22"
           sx={{ height: 2, width: '100%', mt: 4, mb: 4 }}
-        />{' '}
+        />
       </Grid>
 
       <Card variant="outlined">
         <CardHeader
           className="css-4rfrnx-MuiCardHeader-root"
-          //   avatar={<VisibilityOutlinedIcon />}
-          title="View Dyeing "
+          title="View Dyeing"
           titleTypographyProps={{ style: { color: 'white' } }}
-        ></CardHeader>
-
+        />
         <Grid
           container
           spacing={2}
-          width="Inherit"
+          width="inherit"
           sx={{ paddingY: 2, paddingX: 2 }}
         >
           <Grid item xs={12} md={12} paddingTop={1}>
             {/* <ReuseableDataGrid
-                  iColumns={columns}
-                  initialRows={rows}
-                  setInitialData={setInitialData}
-                  deleteApi={deleteApi}
-                  deleteBy="dpId"
-                  refetch={refetchDyeingPrintingData}
-                  setAccordionExpanded={setAccordionExpanded}
-                  fileName="Dyeing&PrintingList"
-                  isLoading={isLoading}
-                /> */}
+              iColumns={columns}
+              initialRows={rows}
+              setInitialData={setInitialData}
+              deleteApi={deleteApi}
+              deleteBy="dpId"
+              refetch={refetchDyeingPrintingData}
+              setAccordionExpanded={setAccordionExpanded}
+              fileName="Dyeing&PrintingList"
+              isLoading={isLoading}
+            /> */}
           </Grid>
         </Grid>
       </Card>
