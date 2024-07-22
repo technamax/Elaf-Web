0a8c3300d25f697ea3502c807987424c58949d09
Mohsin-Shafique <164845524+Mohsin-Shafique@users.noreply.github.com>
2024-07-22 12:25:49 +0500

    no message

--- src/components/ReuseableDataGrid.jsx
+++ src/components/ReuseableDataGrid.jsx
@@ -32,7 +32,8 @@ const ReuseableDataGrid = ({
   disableDelete,
   setIsEdit,
   autoSizeColumns,
-  isLoading
+  isLoading,
+  height
 }) => {
   const apiRef = useGridApiRef();
   const [open, setOpen] = React.useState(false);
@@ -165,7 +166,7 @@ const ReuseableDataGrid = ({
   return (
     <Box
       sx={{
-        height: 500,
+        height: height ? height : 500,
         width: 'inherit',
         '& .actions': {
           color: 'text.secondary'
