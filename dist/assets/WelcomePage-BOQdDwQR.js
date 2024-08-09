70092bce769607e0608376a1573d87ada0d6cd77
Mohsin-Shafique <164845524+Mohsin-Shafique@users.noreply.github.com>
2024-08-09 16:57:20 +0500

    Build Number 2

--- dist/assets/WelcomePage-BOQdDwQR.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-VY4d8e7g.js";import"./vendor-BXCbSo2_.js";function t(){return e.jsx("div",{children:"WelcomePage"})}export{t as default};
---
+++ dist/assets/WelcomePage-ClMNCq_b.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-Cck5hrzL.js";import"./vendor-BXCbSo2_.js";function t(){return e.jsx("div",{children:"WelcomePage"})}export{t as default};
