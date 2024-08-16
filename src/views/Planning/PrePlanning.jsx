dfbc365fe51dc34d3d96e5b21de4094afb95190d
Mohsin-Shafique <164845524+Mohsin-Shafique@users.noreply.github.com>
2024-08-16 10:34:53 +0500

    no message

--- src/views/Planning/PrePlanning.jsx
+++ src/views/Planning/PrePlanning.jsx
@@ -67,8 +67,8 @@ const PrePlanning = ({ setInitialValues, initialValues }) => {
     noOfHeads: 0,
     operatingMachineId: 0,
     planningProcessTypeName: '',
-    repeats: '',
-    repeatSize: '',
+    repeats: 0,
+    repeatSize: 0,
     uomId: '',
     totalFabric: '',
     shrinkage: '',
@@ -108,8 +108,8 @@ const PrePlanning = ({ setInitialValues, initialValues }) => {
       fabricId: initialData?.fabricId || '',
       noOfHeads: initialData?.noOfHeads || 0,
       operatingMachineId: initialData?.operatingMachineId || 0,
-      repeats: initialData?.repeats || '',
-      repeatSize: initialData?.repeatSize || '',
+      repeats: initialData?.repeats || 0,
+      repeatSize: initialData?.repeatSize || 0,
       uomId: initialData?.uomId || '',
       totalFabric: initialData?.totalFabric || '',
       shrinkage: initialData?.shrinkage || '',
@@ -236,7 +236,9 @@ const PrePlanning = ({ setInitialValues, initialValues }) => {
     // }
     // setLoading(false);
   }, [formData.designId, formData.planningHeaderId]);
-  const isDyeing = formData.planningProcessTypeId === 212;
+  const isDyeing =
+    formData.planningProcessTypeId === 212 ||
+    formData.planningProcessTypeId === 1219;
   console.log('initialValues', initialValues);
   console.log('selectedCollectionId', selectedCollectionId);
 
@@ -361,8 +363,8 @@ const PrePlanning = ({ setInitialValues, initialValues }) => {
         noOfHeads: 0,
         operatingMachineId: 0,
 
-        repeats: '',
-        repeatSize: '',
+        repeats: 0,
+        repeatSize: 0,
         uomId: '',
         totalFabric: '',
         shrinkage: '',
@@ -389,8 +391,8 @@ const PrePlanning = ({ setInitialValues, initialValues }) => {
         noOfHeads: 0,
         operatingMachineId: 0,
 
-        repeats: '',
-        repeatSize: '',
+        repeats: 0,
+        repeatSize: 0,
         uomId: '',
         totalFabric: '',
         shrinkage: '',
@@ -414,8 +416,8 @@ const PrePlanning = ({ setInitialValues, initialValues }) => {
         noOfHeads: 0,
         operatingMachineId: 0,
 
-        repeats: '',
-        repeatSize: '',
+        repeats: 0,
+        repeatSize: 0,
         uomId: '',
         totalFabric: '',
         shrinkage: '',
@@ -428,8 +430,9 @@ const PrePlanning = ({ setInitialValues, initialValues }) => {
       setLoading(false);
     } else if (name === 'planningProcessTypeId') {
       const selectedProcess = processType.find(
-        (batch) => batch.planningProcessTypeId === value
+        (batch) => batch.lookUpId === value
       );
+      console.log('selectedProcess', selectedProcess);
       setFormData({
         ...formData,
         planningProcessTypeId: value,
@@ -514,8 +517,8 @@ const PrePlanning = ({ setInitialValues, initialValues }) => {
         colorId: '',
         noOfHeads: 0,
         operatingMachineId: 0,
-        repeats: '',
-        repeatSize: '',
+        repeats: 0,
+        repeatSize: 0,
         uomId: '',
         totalFabric: '',
         total: '',
@@ -669,7 +672,7 @@ const PrePlanning = ({ setInitialValues, initialValues }) => {
           params.value === 'MultiHead'
             ? 'primary.dark'
             : params.value === 'Schiffili'
-              ? 'primary'
+              ? theme.palette.grey[900]
               : params.value === 'Dyeing'
                 ? 'success.dark'
                 : 'default';
