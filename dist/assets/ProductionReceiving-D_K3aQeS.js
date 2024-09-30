03f31b4d7a45a0a1c7ad159905081e305efacf8a
Mohsin-Shafique <164845524+Mohsin-Shafique@users.noreply.github.com>
2024-09-30 09:21:11 +0500

    build saturday

---
+++ dist/assets/ProductionReceiving-D_K3aQeS.js
@@ -0,0 +1,1 @@
+import{j as i}from"./index-39l8sM5Y.js";import"./vendor-BXCbSo2_.js";const t=()=>i.jsx("div",{children:"ProductionReceiving"});export{t as default};
---
+++ dist/assets/WelcomePage-BCl-SkTP.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-39l8sM5Y.js";import"./vendor-BXCbSo2_.js";function t(){return e.jsx("div",{children:"WelcomePage"})}export{t as default};
