15c4bc00f2de8263db96e48ddf81b06f34c65cde
Junaid Ashraf <junaid.ashraf@technamax.com>
2024-10-11 16:59:42 +0500

    build

--- dist/assets/ProductionReceiving-5U9l9CF-.js
+++
@@ -1,1 +0,0 @@
-import{j as i}from"./index-B7j46tQO.js";import"./vendor-BXCbSo2_.js";const t=()=>i.jsx("div",{children:"ProductionReceiving"});export{t as default};
--- dist/assets/WelcomePage-l_hu4crp.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-B7j46tQO.js";import"./vendor-BXCbSo2_.js";function t(){return e.jsx("div",{children:"WelcomePage"})}export{t as default};
