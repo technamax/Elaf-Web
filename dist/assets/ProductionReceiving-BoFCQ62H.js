cd134d7f57e70e6ecbdc8b0c9e28f48aa146fc8c
Mohsin-Shafique <164845524+Mohsin-Shafique@users.noreply.github.com>
2024-10-11 13:49:56 +0500

    build

---
+++ dist/assets/ProductionReceiving-BoFCQ62H.js
@@ -0,0 +1,1 @@
+import{j as i}from"./index-CCiAG5C1.js";import"./vendor-BXCbSo2_.js";const t=()=>i.jsx("div",{children:"ProductionReceiving"});export{t as default};
---
+++ dist/assets/WelcomePage-CDeFEJBQ.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CCiAG5C1.js";import"./vendor-BXCbSo2_.js";function t(){return e.jsx("div",{children:"WelcomePage"})}export{t as default};
